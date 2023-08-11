0e18b082f85aff9d1f2954458535766908d4699c
Hasanat <hasanatmohamed88@gmail.com>
2023-08-10 19:11:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is main
+ *
+ * Return: 0 when it's successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
